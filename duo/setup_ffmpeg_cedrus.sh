21a4660b309d037d4e771662fbb06355fd3af28b
snailium <snailium@hotmail.com>
2018-06-23 05:43:33 -0400

    Added alsa support.

--- duo/setup_ffmpeg_cedrus.sh
+++ duo/setup_ffmpeg_cedrus.sh
@@ -23,13 +23,13 @@ fi
 # https://github.com/uboborov/ffmpeg_h264_H3
 #
 
-$SUDO apt-get install -y libmp3lame-dev libpulse-dev libv4l-dev libx264-dev libvdpau-dev vlc
+$SUDO apt-get install -y libmp3lame-dev libpulse-dev libv4l-dev libx264-dev libvdpau-dev libasound2-dev vlc
 
 git clone -b cedrus264-v2 https://github.com/danielkucera/FFmpeg.git ${CEDRUS_DIR}/ffmpeg
 ${CEDRUS_DIR}/ffmpeg
 
 ./configure --prefix=/usr --enable-nonfree --enable-gpl --enable-version3 --enable-vdpau --enable-libx264 --enable-libmp3lame --enable-libpulse --enable-libv4l2
-make && $SUDO make install
+make -j 4 && $SUDO make install
 
-#ffmpeg -f v4l2 -video_size 1280x720 -i /dev/video0 -pix_fmt nv12 -r 10 -c:v cedrus264 -f mpegts - | cvlc - --sout "#:rtp{sdp=rtsp://:8554/}"
+#ffmpeg -f v4l2 -video_size 1920x1080 -i /dev/video0 -pix_fmt nv12 -r 60 -f alsa -ac 2 -i pulse -acodec aac -strict experimental -c:v cedrus264 -b:v 2M -f mpegts - | cvlc - --sout "#:rtp{sdp=rtsp://:8554/}"
 
